1ee84d7b2ea94803fcb41f545658527e63c51586
Papa Kofi <humblepapa1234@gmail.com>
2023-06-14 15:45:36 +0000

    Text Inputs

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState('Papa Kofi');
+  const [age, setAge] = useState(20);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        onChangeText={(value) => {
+          setName(value);
+        }}
+      />
+
+      <Text>Enter age:</Text>
+      <TextInput
+        inputMode="numeric"
+        style={styles.input}
+        onChangeText={(value) => {
+          setAge(value);
+        }}
+        placeholder="e.g 25"
+      />
+
+      <Text style={{ fontSize: 20 }}>
+        {' '}
+        name: {name} age: {age}{' '}
+      </Text>
     </View>
   );
 }
@@ -13,8 +38,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 190,
   },
 });
