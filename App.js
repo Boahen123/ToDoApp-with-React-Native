554a23b1e3d44456086888a5916329d73f62886f
Papa Kofi <humblepapa1234@gmail.com>
2023-06-15 12:19:02 +0000

    Preparing code for ToDoApp

--- App.js
+++ App.js
@@ -1,72 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'shaun', key: '1' },
-    { name: 'yoshi', key: '2' },
-    { name: 'mario', key: '3' },
-    { name: 'peach', key: '4' },
-    { name: 'frog', key: '5' },
-    { name: 'here', key: '6' },
-    { name: 'toad', key: '7' },
-    { name: 'toad', key: '8' },
-  ]);
-
-  const pressHandler = (key) => {
-    console.log(key);
-    setPeople((prev) => {
-      return prev.filter((person) => person.key != key);
-    });
-  };
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.element}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
-
-/* <ScrollView>
-    {people.map((element) => {
-      return (
-        <View key={element.key}>
-          <Text style={styles.element}>{element.name}</Text>
-        </View>
-      );
-    })}
-  </ScrollView> */
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    backgroundColor: '#fff',
-    paddingHorizontal: 20,
-    paddingTop: 40,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  element: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
   },
 });
